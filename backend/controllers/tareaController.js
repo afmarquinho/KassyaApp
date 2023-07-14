14f8305d50f742903f1ef9fb22cf23580353f580
afmarquinho <fernandez9029@gmail.com>
2023-07-13 23:10:05 -0500

    ok obtener tarea

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -28,7 +28,23 @@ const agregarTarea = async (req, res) => {
 };
 
 const obtenerTarea = async (req, res) => {
-  console.log("desde obtenerTarea");
+  const { id } = req.params;  
+  // METODO POPULATE CRUZA LAS BBDD CONRRESPONDIENTES SEGUN DECLARADO EN EL SCHEMA, PARA MAS CLARIDAD HACER UN CONSOLE.LOG A "tarea" LINEA 34 
+  const tarea = await Tarea.findById(id).populate("proyecto");
+  if(!tarea){
+    const error = new Error(
+      "Tarea no encontrada"
+    );
+    return res.status(404).json({ msg: error.message });
+  }
+  // VALIDAR QUE LAS TAREAS A OBTENER SEAN LAS CREADAS POR EL USUARIO AUTENTICADO
+ if(tarea.proyecto.creador.toString()!== req.usuario._id.toString()){
+  const error = new Error(
+    "Acción no válida -- No tienes permiso de añadir ver estas tareas"
+  );
+  return res.status(403).json({ msg: error.message });
+ }
+ res.json(tarea)
 };
 const actualizarTarea = async (req, res) => {
   console.log("desde actualizarTarea");
